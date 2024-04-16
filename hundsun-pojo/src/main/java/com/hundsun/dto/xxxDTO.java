f4872d45fa79f39d79c9588deb00808cdbfb0a25
wmding <2386585789@qq.com>
2024-04-16 17:45:11 +0800

    基础组件

---
+++ hundsun-pojo/src/main/java/com/hundsun/dto/xxxDTO.java
@@ -0,0 +1,14 @@
+package com.hundsun.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author: Dding
+ * @date: 2024/04/16
+ **/
+@Data
+
+public class xxxDTO implements Serializable {
+}
---
+++ hundsun-pojo/src/main/java/com/hundsun/entity/xxx.java
@@ -0,0 +1,19 @@
+package com.hundsun.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * @author: Dding
+ * @date: 2024/04/16
+ **/
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class xxx implements Serializable {
+}
